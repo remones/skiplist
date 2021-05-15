8bbcda5e83f737cbeb4d317ed60237ff28195bf5
remones <remones11@gmail.com>
2021-05-15 14:10:29 +0800

    revise code architecture

--- src/test/java/com/remones/skiplist/SkipListTest.java
+++ src/test/java/com/remones/skiplist/SkipListTest.java
@@ -2,15 +2,53 @@ package com.remones.skiplist;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Random;
 
-class SkipListTest {
+class Element<T> implements Item<T> {
+    private final double score;
+    private final T value;
+
+    Element(double score, T value) {
+        this.score = score;
+        this.value = value;
+    }
+
+    public boolean less(Item<T> other) {
+        return this.score < other.getScore();
+    };
+
+    public double getScore() {
+        return this.score;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+ }
+
+public class SkipListTest {
     /**
      * Rigorous Test.
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void testInsert() {
+        SkipList<String> sl = new SkipList<>(10);
+        for (int i = 0; i < 10; i++) {
+            Element<String> e = new Element<>((double)i, String.format("value-%d", i));
+            sl.insert(e);
+        }
+        sl.print();
+    }
+
+    @Test
+    public void testGet() {
+
+    }
+
+    @Test
+    public void testPop() {
+
     }
 }
